cb3338edcad46a33f168376653040aac4efa9c50
abdallah dadikhy <abdallahdadikhy@gmail.com>
2025-06-11 16:21:23 +0300

    student profile

--- app/Models/StudentProfile.php
+++ app/Models/StudentProfile.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+
 
 class StudentProfile extends Model
 {
@@ -19,7 +21,16 @@ class StudentProfile extends Model
         'photo_path',
         'university_id'
     ];
+    protected $appends = ['photo_path_url'];
+    public function getPhotoPathUrlAttribute() // تم تغيير اسم الدالة لتطابق السمة في $appends
+    {
+        $value = $this->attributes['photo_path']; // الوصول إلى القيمة الخام المخزنة في قاعدة البيانات
 
+        if ($value && Storage::disk('public')->exists($value)) {
+            return asset('storage/' . $value);
+        }
+        return null;
+    }
     public function student()
 {
     return $this->belongsTo(Student::class, 'university_id', 'university_id');
